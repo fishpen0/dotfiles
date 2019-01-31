8fb3e12cdd40ca0ba3932b332bcbee946676a548
fishpen0 <fishpen0@users.noreply.github.com>
2019-01-31 14:39:26 -0500

    Drop support for anything but macos

--- init.sh
+++ init.sh
@@ -5,20 +5,6 @@ run_generic() {
   generic/init.sh
 }
 
-# Do Debian Tasks
-run_debian() {
-  echo "Debian based OS Detected."
-  debian/init.sh
-  run_generic
-}
-
-# Do RHEL Tasks
-run_rhel() {
-  echo "Rhel based OS Detected."
-  rhel/init.sh
-  run_generic
-}
-
 # Do OSX Tasks
 run_osx() {
   echo "Mac OS Detected."
@@ -28,27 +14,4 @@ run_osx() {
   run_generic
 }
 
-# Detect OS
-unsupported() {
-  echo "\nUnsupported OS! Exiting...\n" >&2
-  exit 1
-}
-
-detect_distro() {
-  # TODO: detect rhel v deb
-  echo "distro"
-}
-
-detect_os() {
-  case "$OSTYPE" in
-    linux*)   detect_distro ;;
-    darwin*)  run_osx ;; 
-    win*)     run_windows ;;
-    cygwin*)  ;&
-    bsd*)     ;&
-    solaris*) ;&
-    *)        unsupported ;;
-  esac
-}
-
-detect_os
+run_osx
