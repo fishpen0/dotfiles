e7cd439dee9f326d0001faa364d67409454b2354
fishpen0 <fishpen0@users.noreply.github.com>
2019-01-31 14:51:37 -0500

    Further work to restructure dotfiles repo for just macos

--- generic/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Running Generic Tasks"
-echo "Copying Generic Dotfiles"
-cd ./dotfiles/
-cp -vR . ~/
-
-echo "Installing crontab"
-crontab ~/.cron
--- init.sh
+++ init.sh
@@ -1,17 +1,86 @@
-#!/usr/bin/env bash
+#! /bin/sh
 
-# Do Generic Tasks
-run_generic() {
-  generic/init.sh
-}
+echo "Running MacOs Tasks"
 
-# Do OSX Tasks
-run_osx() {
-  echo "Mac OS Detected."
-  cd mac
-  #./init.sh
-  cd ../
-  run_generic
-}
+# Enable xcode clt
+xcode-select --install
 
-run_osx
+############
+# Homebrew #
+############
+
+# Install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Disable Tracking
+brew analytics off
+
+# Setup cask for applications
+brew tap caskroom/cask
+
+# Setup mas for app store support
+brew install mas
+
+# Install Brewfile
+brew bundle 
+
+# Done
+brew upgrade
+brew cleanup
+
+#########
+# Iterm #
+#########
+
+# Copy Iterm Configs
+cp -r .iterm2/ ~/.iterm2/
+cp .iterm2_* ~/
+
+# Install Iterm Tab Setter
+npm install -g iterm2-tab-set
+
+##########
+# Mac OS #
+##########
+
+# Execute Mac OS configuration tasks
+source .macos
+
+###############
+# Shell Stuff #
+###############
+
+# Suppress osx motd
+touch ~/.hushlogin
+
+# Fix Bash to default to bash 4
+echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+chsh -s /usr/local/bin/bash;
+
+# Install powerline fonts
+unzip ../fonts/Inconsolata.zip -d /Library/Fonts
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Downloading latest iterm integration files for bash"
+curl -sL https://iterm2.com/shell_integration/bash -o ~/.iterm2_shell_integration.bash
+
+echo "Downloading latest iterm integration files for zsh"
+curl -sL https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
+
+######################
+# Visual-studio-code #
+######################
+
+# Install extensions
+while IFS='' read -r line || [[ -n "$extension" ]]; do
+    code --install-extension $extension
+done < "../generic/vscode/extensions"
+
+###########
+# crontab #
+###########
+
+echo "Installing crontab"
+crontab ~/.cron
